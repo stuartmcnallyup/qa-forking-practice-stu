9e9177b81d7f1f55e9ee26252df34df2359897b5
stuartmcnallyup <126587116+stuartmcnallyup@users.noreply.github.com>
2026-03-17 12:27:53 +0000

    adding bug

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 def greet(name):
     """Return a greeting for the given name."""
-    return f"Hello, {name}! Welcome to DevOps."
+    return f"Hello, {nam}! Welcome to DevOps."
+    
 
 
+    
 if __name__ == "__main__":
     print(greet("World"))
